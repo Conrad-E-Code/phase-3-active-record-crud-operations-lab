19bd92e333dd02a60c462f8073b3deb323ef5c53
Conrad Etherington <conrad.etherington@gmail.com>
2022-11-30 21:34:55 -0700

    commit tree 0d7a044c7409997161f593b7573156c070f038a7

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,3 +1,36 @@
 class Movie < ActiveRecord::Base
-
+    def self.create_with_title title
+        a = self.create :title => title
+    end
+    def self.first_movie
+        Movie.first
+    end
+    def self.last_movie
+        Movie.last
+    end
+    def self.movie_count
+        Movie.count
+    end
+    def self.find_movie_with_id(id)
+        Movie.find(id)
+    end
+    def self.find_movie_with_attributes(args)
+        Movie.find_by(args)
+    end
+    def self.find_movies_after_2002
+        Movie.where("release_date > 2002")
+    end
+    def update_with_attributes(args)
+        self.update(args)
+    end
+    def self.update_all_titles title
+        Movie.update_all title: title
+    end
+    def self.delete_by_id id
+        a = Movie.find_movie_with_id(id)
+        a.delete
+    end
+    def self.delete_all_movies
+        Movie.delete_all
+    end
 end
